6380ca0ecf5550f60178a775a1b3d0898ca83c69
UniversalMonkeyy <jobexperts@icloud.com>
2024-11-06 20:48:27 +0100

    Add files via upload

---
+++ techno.py
@@ -0,0 +1,54 @@
+Python 3.13.0 (v3.13.0:60403a5409f, Oct  7 2024, 00:37:40) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('''                          .    . ///
+...                       ____J)   (//<_
+...                        -__ l   Y .--'
+...                           \ ` / /
+...                            ` / '   |\  __
+...                             ' /. (`  `)  `-.
+...                            (  `. /  o2 _    )
+...                 a8.         \   `-.--.  `- /
+...                 888          \_.   ) `._.'
+...                88P'        +_.)_   .              88a.
+...      8a       888( .a88   *_.'  `==)             888888a
+...      `8ba.ad8888888888P    `...-'  '            888888888
+...        Y888P88888             )  ./            88888888888
+...            a88888             (   (           88P 88888 88)
+...           888888              *_.' \          P  888888 8P
+...          88Y'888           .-  )  ` \            888888
+...        a88"   88          (_.-'  ) ) )           888888
+...       888'    Y88     .-"" `. .-'.' .          a88 `88a
+...        Y8b.    `88   '       )   .-'          8P8   `88a
+...      aa8888     888. \ ' _._.--"" `         888P      Y8P
+...                       `.   `. `-.  `                    "
+...                         `.   `   `._)
+...                           `.  `. '( )
+...  a:f___________________     `.--)`|_\   ___________________________
+...                           __//_ )
+...                         (:___/ #_)'
+...                         ''')
+... print("Witaj na Techno imprezie")
+... print("Twoje zadanie to przeżyć")
+... gdziejestes=input("Jest piątek, siedzicie na chacie i wpadają teściowie, mowia, ze zostana z Demi caly weekend, pada pomysł, zeby jechać na techno- WCHODZISZ w to czy ZOSTAJESZ NA CHACIE?\n")
+... 
+... if gdziejestes==("wchodze"):
+...     print("Legancko, w takim razie pakujecie manatki, stringi, miniówy i lecicie. Wsiadacie w auto i dojezdzacie na miejsce")
+... else:
+...     print("Hah, ale słabizna, Majkel łapie Cie za nogi i zaciąga do auta, odpala Skode i po trzech godzinach dojezdżacie do Berlina")
+... 
+... conajpierw=input("Jesteście w Berlinie, wieczor zapadł, stoicie gdzieś na ulicy, wiecie, że trzeba sie najebac i wiecie, ze trzeba sie przekimać. Co najpierw, załatwiç ALKO czy SPANIE")
+... if conajpierw==("alko"):
+...     print("Wiadomo, najważniejsza najebka.")
+... elif conajpierw==("spanie"):
+...     print("ok")
+... print("Po 10 minutach dotarliście do monopolowego ale przed wejściem zaczepiają was Turasy, jeden wyciaga kose i każe Majkelowi oddać cały hajs. Co ma zrobić: ODDAC pieniądze czy WALCZYĆ")
+... 
+... if input("oddac"):
+...     print("Jajebie, chyba sobie żarty robisz że ma im dobrowolnie kase oddac. Majkel Cie nie slucha i zajebał mu kopa w ryj. Reszta turkow sie zesrała i poszła w długą")
+... elif input("walczyc"):
+...     print("Majkel podchodzi do niego i wykręca mu kose z ręki. Turek zawzięcie walczy ale Majkelowi udało się zabrać mu nóż, Turków ogarnęła panika i uciekają")
+... 
+... # Teraz potrzebujecie miejsca, zeby sie wykimać, Co wybieracie- HILTON czy TANI HOSTEL?
+... # Tani Hosel
+... # ojapierdole, tani hostel z turkami i karaluchami. No ale dobra, lepiej zachować hajs na ćpanie
+... #  Jesteście już na miejscu, przebrani
